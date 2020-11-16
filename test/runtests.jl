a877f70d84a92cca044cd6435437d8369ae97052
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2020-11-16 07:32:24 +0100

    Added simple test of loading a file with CIS excited states/dipoles

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,42 @@
 using Gamess
 using Test
 
+load_file(name) = Gamess.load(joinpath(dirname(@__FILE__), "data", "$(name).out"))
+
 @testset "Gamess.jl" begin
-    # Write your tests here.
+    @testset "Water CIS" begin
+        water = load_file("water")
+        atoms = water.molecule.atoms
+        @test size(atoms) == (3, 5)
+        @test atoms.Atom == ["OXYGEN", "HYDROGEN", "HYDROGEN"]
+
+        ci = water.ci
+        states = ci.excited_states
+        @test size(states) == (100,3)
+
+        dipoles = ci.length_gauge_dipole
+        @test size(dipoles) == (2601,10)
+        @test dipoles[1, :z] ≈ 0.958273779741262
+        @test dipoles[2599,:z] ≈ 0.044333
+        @test dipoles[2599,:A] ≈ -5.4312e-41
+        @test dipoles[2599,:B] ≈ 797650.0
+    end
+
+    @testset "Neon CIS" begin
+        neon = load_file("ne+-guga")
+        atoms = neon.molecule.atoms
+        @test size(atoms) == (1, 5)
+        @test atoms.Atom == ["NE"]
+
+        ci = neon.ci
+        states = ci.excited_states
+        @test size(states) == (5,2)
+
+        dipoles = ci.length_gauge_dipole
+        @test size(dipoles) == (10,10)
+        @test dipoles[8, :z] ≈ -0.521326
+
+        dipoles = ci.velocity_gauge_dipole
+        @test size(dipoles) == (10,8)
+    end
 end
